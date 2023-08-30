689aed2c65a150ac00e65fc20e33fb1c24072ba3
u237856 <ernst.pluess2@sbb.ch>
2023-08-30 18:33:25 +0200

    websocket communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <ESPmDNS.h>
 #include <SPIFFS.h>
 #include <WebServer.h>
+#include <WebSocketsServer.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 #define LED 2
+#define USE_SERIAL Serial
 
 vector<String> readCredentials() {
   File file = SPIFFS.open("/credentials.txt", "r");
@@ -34,8 +39,72 @@ vector<String> readCredentials() {
   return v;
 }
 
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+  const uint8_t *src = (const uint8_t *)mem;
+  USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)",
+                    (ptrdiff_t)src, len, len);
+  for (uint32_t i = 0; i < len; i++) {
+    if (i % cols == 0) {
+      USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    USE_SERIAL.printf("%02X ", *src);
+    src++;
+  }
+  USE_SERIAL.printf("\n");
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload,
+                    size_t length) {
+
+  switch (type) {
+  case WStype_DISCONNECTED:
+    USE_SERIAL.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED: {
+    IPAddress ip = webSocket.remoteIP(num);
+    USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0],
+                      ip[1], ip[2], ip[3], payload);
+
+    // send message to client
+    webSocket.sendTXT(num, "Connected");
+  } break;
+  case WStype_TEXT:
+    USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+    // send message to client
+    webSocket.sendTXT(num, "received a message");
+
+    // send data to all connected clients
+    // webSocket.broadcastTXT("message here");
+    break;
+  case WStype_BIN:
+    USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
+    hexdump(payload, length);
+
+    // send message to client
+    // webSocket.sendBIN(num, payload, length);
+    break;
+  case WStype_ERROR:
+  case WStype_FRAGMENT_TEXT_START:
+  case WStype_FRAGMENT_BIN_START:
+  case WStype_FRAGMENT:
+  case WStype_FRAGMENT_FIN:
+    break;
+  }
+}
+
 void handleHello() {
   File file = SPIFFS.open("/hello.html", "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  server.streamFile(file, "text/html");
+  file.close();
+}
+
+void websocketHtmlPage() {
+  File file = SPIFFS.open("/websocket.html", "r");
   if (!file) {
     Serial.println("Failed to open file for reading");
     return;
@@ -101,6 +170,7 @@ void setup(void) {
   server.on("/", handleRoot);
 
   server.on("/hello", handleHello);
+  server.on("/websocket", websocketHtmlPage);
 
   server.on("/inline",
             []() { server.send(200, "text/plain", "this works as well"); });
@@ -109,9 +179,15 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.println("WebSocket server started");
 }
 
 void loop(void) {
   server.handleClient();
+  webSocket.loop();
+
   delay(2); // allow the cpu to switch to other tasks
 }
